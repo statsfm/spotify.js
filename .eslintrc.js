ecc191532ed65ddaa12ef8e8eca2853f764e7183
Sting Alleman <stingalleman@icloud.com>
2021-08-21 17:32:15 +0100

    chroe: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier']
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    project: ['./tsconfig.json']
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['@tribecamp/base', '@tribecamp/typescript', 'prettier'],
+  rules: { 'prettier/prettier': 'error' }
 };
