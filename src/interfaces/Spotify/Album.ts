578121059d9af65bd3d1ea76f71e22effba19236
Sting Alleman <stingalleman@icloud.com>
2021-08-26 14:17:54 +0100

    fix: fix types, add documentation

--- src/interfaces/Spotify/Album.ts
+++ src/interfaces/Spotify/Album.ts
@@ -1,19 +1,91 @@
 import { Artist } from './Artist';
 import { ExternalUrls } from './shared/ExternalUrls';
 import { Image } from './shared/Image';
+import { Track } from './Track';
 
 export interface SimplifiedAlbum {
-  album_type: string;
+  /**
+   * The type of the album.
+   */
+  album_type: 'album' | 'single' | 'compilation';
+  /**
+   * The artists of the album.
+   */
   artists: Artist[];
+  /**
+   * The markets in which the album is available: [ISO 3166-1 alpha-2](http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) country codes.
+   * Note that an album is considered available in a market when at least 1 of its tracks is available in that market.
+   */
   available_markets: string[];
+  /**
+   * Known external URLs for this album.
+   */
   external_urls: ExternalUrls;
+  /**
+   * A link to the Web API endpoint providing full details of the album.
+   */
   href: string;
+  /**
+   * The Spotify ID for the album.
+   */
   id: string;
+  /**
+   * The cover art for the album in various sizes, widest first.
+   */
   images: Image[];
-  name: string;
+  /**
+   * The name of the album.
+   * In case of an album takedown, the value may be an empty string.
+   */
+  name?: string;
+  /**
+   * The date the album was first released.
+   */
   release_date: string;
-  release_date_precision: string;
+  /**
+   * The precision with which `release_date` value is known.
+   */
+  release_date_precision: 'year' | 'month' | 'day';
+  /**
+   * The number of tracks in the album.
+   */
   total_tracks: number;
-  type: string;
+  /**
+   * The object type.
+   */
+  type: 'album';
+  /**
+   * The Spotify URI for the album.
+   */
   uri: string;
+  /**
+   * **The field is present when getting an artist's albums.**
+   * Compare to album_type this field represents relationship between the artist and the album.
+   */
+  album_group?: 'album' | 'single' | 'compilation' | 'appears_on';
+}
+
+export interface Album extends SimplifiedAlbum {
+  /**
+   * Included in the response when a content restriction is applied.
+   */
+  restrictions?: {
+    /**
+     * The reason for the restriction.
+     * Albums may be restricted if the content is not available in a given market, to the user's subscription type, or when the user's account is set to not play explicit content.
+     */
+    reason: 'market' | 'product' | 'explicit';
+  }[];
+  /**
+   * The tracks of the album.
+   */
+  tracks: {
+    href: string;
+    items: Track[];
+    limit: number;
+    next: string;
+    offset: number;
+    previous: string;
+    total: number;
+  };
 }
--- src/interfaces/Spotify/Track.ts
+++ src/interfaces/Spotify/Track.ts
@@ -4,21 +4,79 @@ import { ExternalIds } from './shared/ExternalIds';
 import { ExternalUrls } from './shared/ExternalUrls';
 
 export interface Track {
+  /**
+   * The album on which the track appears.
+   */
   album: SimplifiedAlbum;
+  /**
+   * The artists who performed the track.
+   */
   artists: Artist[];
+  /**
+   * The markets in which the album is available: [ISO 3166-1 alpha-2](http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2) country codes.
+   * Note that an album is considered available in a market when at least 1 of its tracks is available in that market.
+   */
   available_markets: string[];
+  /**
+   * The disc number (usually 1 unless the album consists of more than one disc).
+   */
   disc_number: number;
+  /**
+   * The track length in milliseconds.
+   */
   duration_ms: number;
+  /**
+   * Whether or not the track has explicit lyrics.
+   */
   explicit: boolean;
+  /**
+   * Known external IDs for the track.
+   */
   external_ids: ExternalIds;
+  /**
+   * Known external URLs for this track.
+   */
   external_urls: ExternalUrls;
+  /**
+   * A link to the Web API endpoint providing full details of the track.
+   */
   href: string;
+  /**
+   * The Spotify ID for the track.
+   */
   id: string;
+  /**
+   * Included in the response when a content restriction is applied.
+   */
+  restrictions: {
+    reason: 'market' | 'product' | 'explicit';
+  };
+  /**
+   * Whether or not the track is from a local file.
+   */
   is_local: boolean;
+  /**
+   * Name of the track.
+   */
   name: string;
+  /**
+   * The popularity of the track. The value will be between 0 and 100, with 100 being the most popular.
+   */
   popularity: number;
-  preview_url: string;
+  /**
+   * A link to a 30 second preview (MP3 format) of the track.
+   */
+  preview_url?: string;
+  /**
+   * The number of the track. If an album has several discs, the track number is the number on the specified disc.
+   */
   track_number: number;
-  type: string;
+  /**
+   * The object type: "track".
+   */
+  type: 'track';
+  /**
+   * The Spotify URI for the track.
+   */
   uri: string;
 }
