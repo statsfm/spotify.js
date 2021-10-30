18f9e421b14f32a69a0cb1e08bb3fc99f437cc25
Sting Alleman <stingalleman@icloud.com>
2021-10-30 21:40:15 +0200

    feat: make config accessible

--- src/lib/SpotifyAPI.ts
+++ src/lib/SpotifyAPI.ts
@@ -24,13 +24,16 @@ export class SpotifyAPI {
 
   private privateConfig: PrivateConfig = {};
 
+  config: SpotifyConfig;
+
   constructor(config: SpotifyConfig) {
-    this.tracks = new TrackManager(config, this.privateConfig);
-    this.albums = new AlbumManager(config, this.privateConfig);
-    this.artists = new ArtistManager(config, this.privateConfig);
-    this.users = new UserManager(config, this.privateConfig);
-    this.me = new MeManager(config, this.privateConfig);
-    this.search = new SearchManager(config, this.privateConfig);
-    this.audio = new AudioManager(config, this.privateConfig);
+    this.config = config;
+    this.tracks = new TrackManager(this.config, this.privateConfig);
+    this.albums = new AlbumManager(this.config, this.privateConfig);
+    this.artists = new ArtistManager(this.config, this.privateConfig);
+    this.users = new UserManager(this.config, this.privateConfig);
+    this.me = new MeManager(this.config, this.privateConfig);
+    this.search = new SearchManager(this.config, this.privateConfig);
+    this.audio = new AudioManager(this.config, this.privateConfig);
   }
 }
