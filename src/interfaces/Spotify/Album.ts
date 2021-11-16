e0eb051e4c052f2bc9fc4388adada8bae8914c70
Sting Alleman <stingalleman@icloud.com>
2021-11-16 18:37:18 +0100

    fix: add popularity to album interface

--- src/interfaces/Spotify/Album.ts
+++ src/interfaces/Spotify/Album.ts
@@ -64,6 +64,10 @@ export interface AlbumBase {
    * The Spotify URI for the album.
    */
   uri: string;
+  /**
+   * The popularity of the artist. The value will be between 0 and 100, with 100 being the most popular. The artist's popularity is calculated from the popularity of all the artist's tracks.
+   */
+  popularity: number;
 }
 
 export interface AlbumSimplified extends AlbumBase {
